eb7bff5759a594ac6e8ed0e8381e93b6d3171f69
chandrakant <chandrakant.r.nagpure@gmail.com>
2026-05-05 11:38:51 +0530

    Install dotenv

--- mail/submit-form.php
+++ mail/submit-form.php
@@ -5,7 +5,12 @@ use PHPMailer\PHPMailer\Exception;
 
 require '../vendor/autoload.php';
 
-$conn = new mysqli("localhost", "root", "", "westdenver");
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Database Connection Failed");
